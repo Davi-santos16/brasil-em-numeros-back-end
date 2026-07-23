9aba395cd795aed5312d3bcf9c11409a5d32a06b
Davi-santos16 <davicastro213@gmail.com>
2026-07-22 21:35:04 -0300

    cors

--- src/controller/indicador.controller.ts
+++ src/controller/indicador.controller.ts
@@ -7,31 +7,22 @@ import axios from "axios";
 export class IndicadorController {
   async index(request: Request, response: Response) {
     const { indicador, regiao } = request.query;
-  
-    try {
 
-      const urlAPI = process.env.API_DADOS_URL
-
-
-
-      const respostaAPI = await axios.get(`${urlAPI}/grafico`, {
-        params: {
-          indicador,
-          regiao
-        }
-      });
-      
-      const dadosDaEquipe = respostaAPI.data;
-
- 
-      return response.status(200).json({dadosDaEquipe});
-      
-    } catch (error) {
-
-      console.error("Erro na comunicação com a API de dados:", error);
-      return response.status(500).json({ error: "Erro ao buscar dados na API externa" });
+    if (!indicador) {
+      throw new AppError("O indicador é obrigatório")
     }
 
+    const urlAPI = process.env.API_DADOS_URL;
 
+    const respostaAPI = await axios.get(`${urlAPI}/grafico`, {
+      params: {
+        indicador,
+        regiao,
+      },
+    });
+
+    const dadosDaEquipe = respostaAPI.data;
+
+    return response.status(200).json({ dadosDaEquipe });
   }
-} 
+}
